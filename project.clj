cb5617fdb0ccab660b130a4d3cd59b7dce3d691b
Duncan McGreggor <oubiwann@gmail.com>
2017-02-11 16:11:02 -0600

    Updated formatting.

--- project.clj
+++ project.clj
@@ -1,35 +1,35 @@
 (defproject clojusc/env-ini "0.3.0-SNAPSHOT"
   :description "Clojure(script) functions for accessing config data from the ENV or INI files"
   :url "https://github.com/clojusc/env-ini"
-  :license
-    {:name "Apache License, Version 2.0"
-     :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies
-    [[org.clojure/clojure "1.8.0"]
-     [org.clojure/clojurescript "1.9.473"]
-     [clojure-ini "0.0.2"]
-     [clojusc/cljs-tools "0.1.2"]]
-  :plugins
-    [[lein-cljsbuild "1.1.5"]
-     [lein-npm "0.6.2"]]
-  :npm
-    {:dependencies
-      [[ini "1.3.4"]]}
+  :license {
+    :name "Apache License, Version 2.0"
+    :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [org.clojure/clojurescript "1.9.473"]
+    [clojure-ini "0.0.2"]
+    [clojusc/cljs-tools "0.1.2"]]
+  :plugins [
+    [lein-cljsbuild "1.1.5"]
+    [lein-npm "0.6.2"]]
+  :npm {
+    :dependencies [
+      [ini "1.3.4"]]}
   :clean-targets ^{:protect false}
     ["resources/public/js"
      "target"]
   :cljsbuild {
-    :builds [
-      {:id "env-ini"
-       :compiler
-         {:main "env-ini.core"
-          :output-to "resources/public/js/env_ini.js"
-          :output-dir "resources/public/js"}}
-      {:id "node"
-       :compiler
-         {:target :nodejs
-          :output-to "target/node/env_ini.js"
-          :output-dir "target/node"}}]}
+    :builds [{
+      :id "env-ini"
+      :compiler {
+        :main "env-ini.core"
+        :output-to "resources/public/js/env_ini.js"
+        :output-dir "resources/public/js"}} {
+      :id "node"
+      :compiler {
+        :target :nodejs
+        :output-to "target/node/env_ini.js"
+        :output-dir "target/node"}}]}
   :aliases {
     "rhino-repl"
       ^{:doc "Start a Rhino-based Clojurescript REPL"}
@@ -42,9 +42,11 @@
     "browser-repl"
       ^{:doc "Start a browser-based Clojurescript REPL"}
       ["trampoline" "run" "-m" "clojure.main"
-       "dev-resources/src/clj/clojusc/env_ini/browser-dev.clj"]
-     }
+       "dev-resources/src/clj/clojusc/env_ini/browser-dev.clj"]}
   :profiles {
+    :uberjar {
+      :aot :all
+      :source-paths ["src" "test/clj"]}
     :test {
       :plugins [
         [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
