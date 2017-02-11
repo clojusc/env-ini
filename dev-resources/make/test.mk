6c8ba16d1fca81c999950a9ab7b088147354d924
Duncan McGreggor <oubiwann@gmail.com>
2017-02-11 17:39:47 -0600

    Added test for reading config files from jar files.

--- dev-resources/make/test.mk
+++ dev-resources/make/test.mk
@@ -1,2 +1,7 @@
 check:
 	lein test :all
+
+check-jar-config:
+	@lein uberjar
+	java -cp target/env-ini-$(VERSION)-standalone.jar \
+	clojusc.env_ini_test show-example-ini
--- test/clj/clojusc/env_ini_test.clj
+++ test/clj/clojusc/env_ini_test.clj
@@ -1,3 +1,13 @@
 (ns clojusc.env-ini-test
-  (:require [clojure.test :refer :all]
-            [clojusc.env-ini.core :as env-ini]))
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]]
+            [clojure.test :refer :all]
+            [clojure-ini.core :as config-ini]
+            [clojusc.env-ini.core :as env-ini]
+            [clojusc.env-ini.ini :as ini])
+  (:gen-class))
+
+(defn -main
+  [mode & args]
+  (case (keyword mode)
+    :show-example-ini (pprint (ini/read "example-config.ini"))))
