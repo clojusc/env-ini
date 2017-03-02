74b2142b18607a7fa3d285dda1e17cafbfa0c637
Duncan McGreggor <oubiwann@gmail.com>
2017-03-02 14:50:32 -0600

    Bumped version for next dev cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/env-ini "0.3.1-SNAPSHOT"
+(defproject clojusc/env-ini "0.4.0-SNAPSHOT"
   :description "Clojure(script) functions for accessing config data from the ENV or INI files"
   :url "https://github.com/clojusc/env-ini"
   :license {
